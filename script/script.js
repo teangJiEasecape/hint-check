8c7c97f933089df4e77413b9d9492215dc88ecd9
Kim ji yeon <w1dus@hanmail.net>
2025-12-30 22:52:31 +0900

    대소문자 구분 삭제

--- script/script.js
+++ script/script.js
@@ -5,8 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
   btn.addEventListener('click', () => {
     const answer = "A9F!Q2@KR#7MX$4ZP8";
 
-    // 대소문자 구분 없이 비교
-    if (input.value.trim().toUpperCase() === answer.toUpperCase()) {
+    // 공백 제거 + 대소문자 통일 + 이상한 문자 제거
+    const userValue = input.value
+      .trim()
+      .replace(/\s/g, '')
+      .toUpperCase();
+
+    const correctValue = answer.toUpperCase();
+
+    if (userValue === correctValue) {
       $('main .result-box').addClass('show');
       $('main .failure-box').removeClass('show');
     } else {
