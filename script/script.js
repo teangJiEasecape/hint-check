b6bbb6941146267fb817735186fcca52577bade8
Kim ji yeon <w1dus@hanmail.net>
2025-12-30 22:49:41 +0900

    대소문자 구분 삭제

--- script/script.js
+++ script/script.js
@@ -5,12 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
   btn.addEventListener('click', () => {
     const answer = "A9F!Q2@KR#7MX$4ZP8";
 
-    if (input.value === answer) {
-      $('main .result-box').addClass('show')
-      $('main .failure-box').removeClass('show')
+    // 대소문자 구분 없이 비교
+    if (input.value.trim().toUpperCase() === answer.toUpperCase()) {
+      $('main .result-box').addClass('show');
+      $('main .failure-box').removeClass('show');
     } else {
-      $('main .result-box').removeClass('show')
-      $('main .failure-box').addClass('show')
+      $('main .result-box').removeClass('show');
+      $('main .failure-box').addClass('show');
     }
   });
 });
